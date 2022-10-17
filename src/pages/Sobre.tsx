acf436a3dc441139e9d17d70873fef1742aadf1d
fescarvalho <fernando_scarvalho@hotmail.com>
2022-10-17 15:15:44 -0300

    creation page of about

--- src/pages/Sobre.tsx
+++ src/pages/Sobre.tsx
@@ -1,8 +1,11 @@
+import { FaArrowDown } from "react-icons/fa";
+import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
+import "./Sobre.css";
 
 const Sobre = () => {
   return (
-    <div>
+    <div className="sobre">
       <Header
         title="Sobre Mim"
         nameOne="Home"
@@ -10,6 +13,31 @@ const Sobre = () => {
         toOne="/"
         toTwo="/projetos"
       />
+
+      <div className="content">
+        <div className="text">
+          <div className="curriculo">
+            <a href="">
+              <FaArrowDown className="iconDown" /> Currículo
+            </a>
+          </div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti nulla sint
+            sed quas, recusandae eligendi accusamus tempore in, reiciendis adipisci quidem
+            optio asperiores eaque doloremque molestias nihil nisi sunt explicabo? Lorem
+            ipsum dolor, sit amet consectetur adipisicing elit. Deleniti nulla sint sed
+            quas, recusandae eligendi accusamus tempore in, reiciendis adipisci quidem
+            optio asperiores eaque doloremque molestias nihil nisi sunt explicabo? Lorem
+            ipsum dolor, sit amet consectetur adipisicing elit. Deleniti nulla sint sed
+            quas, recusandae eligendi accusamus tempore in, reiciendis adipisci quidem
+            optio asperiores eaque doloremque molestias nihil nisi sunt explicabo? Lorem
+            ipsum dolor, sit amet consectetur adipisicing elit. Deleniti nulla sint sed
+            quas, recusandae eligendi accusamus tempore in, reiciendis adipisci quidem
+            optio asperiores eaque doloremque molestias nihil nisi sunt explicabo? Lorem
+          </p>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
