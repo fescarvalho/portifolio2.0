59864ab6f6610c30763b242df40066767b1613e7
fescarvalho <fernando_scarvalho@hotmail.com>
2022-10-17 15:33:01 -0300

    creation page of about

--- src/pages/Sobre.tsx
+++ src/pages/Sobre.tsx
@@ -5,40 +5,37 @@ import "./Sobre.css";
 
 const Sobre = () => {
   return (
-    <div className="sobre">
-      <Header
-        title="Sobre Mim"
-        nameOne="Home"
-        nameTwo="Projetos"
-        toOne="/"
-        toTwo="/projetos"
-      />
+    <>
+      <div className="sobre">
+        <Header
+          title="Sobre Mim"
+          nameOne="Home"
+          nameTwo="Projetos"
+          toOne="/"
+          toTwo="/projetos"
+        />
 
-      <div className="content">
-        <div className="text">
-          <div className="curriculo">
-            <a href="">
-              <FaArrowDown className="iconDown" /> Currículo
-            </a>
+        <div className="content">
+          <div className="text">
+            <div className="curriculo">
+              <a
+                href="../../public/Currículo(Dev) - Fernando da Silva Carvalho (1).pdf"
+                download
+              >
+                <FaArrowDown className="iconDown" /> Currículo
+              </a>
+            </div>
+            <p>
+              Ola, sou um aficionado por tecnologia, buscando uma transição de carreira
+              para área. Possuo sólidos conhecimentos em tecnologias, voltadas para o
+              desenvolvimento web, como Javascript, ReactJs, Typescript e NodeJs. Sempre
+              em contínuo aprendizado e atualização.
+            </p>
           </div>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti nulla sint
-            sed quas, recusandae eligendi accusamus tempore in, reiciendis adipisci quidem
-            optio asperiores eaque doloremque molestias nihil nisi sunt explicabo? Lorem
-            ipsum dolor, sit amet consectetur adipisicing elit. Deleniti nulla sint sed
-            quas, recusandae eligendi accusamus tempore in, reiciendis adipisci quidem
-            optio asperiores eaque doloremque molestias nihil nisi sunt explicabo? Lorem
-            ipsum dolor, sit amet consectetur adipisicing elit. Deleniti nulla sint sed
-            quas, recusandae eligendi accusamus tempore in, reiciendis adipisci quidem
-            optio asperiores eaque doloremque molestias nihil nisi sunt explicabo? Lorem
-            ipsum dolor, sit amet consectetur adipisicing elit. Deleniti nulla sint sed
-            quas, recusandae eligendi accusamus tempore in, reiciendis adipisci quidem
-            optio asperiores eaque doloremque molestias nihil nisi sunt explicabo? Lorem
-          </p>
         </div>
       </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
